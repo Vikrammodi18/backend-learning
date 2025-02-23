f18544667ede25cd0fc8f475fbd13fd7c0eaa387
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-02-23 20:52:47 +0530

    added multer and cloudinary setup

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ const {DB_NAME} = require('../constants')
 require('dotenv').config()
 
 const connectDB = async ()=>{
-    
+    console.log("hello")
     try {
        const connectionInstance= await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
        console.log("connection is successful,DB_HOST:",connectionInstance.connection.host)
