3c3accce7456f51b76784e7d57c85ccde5d219bb
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-03-08 03:36:12 +0530

    add update comment feature; implement updateComment controller and route for modifying existing comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,7 +7,70 @@ const {mongoose,isValidObjectId} = require("mongoose")
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {page = 1, limit = 10} = req.query
-    
+    if(page<1){
+        throw new ApiError(400,"page start from 1")
+    }
+    const {videoId} = req.params;
+    if(!videoId){
+        throw new ApiError(400,"videoId are required")
+    }
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"videoId are required")
+    }
+    const comments = await Comment.aggregate([
+         {
+            $match:{
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+         },
+         {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                pipeline:[
+                {
+                      $project:{
+                        username:1,
+                        avatar:1,
+                        createdAt:1
+                    }
+                },
+                ],
+                as:"owner"
+            }
+         },
+         {
+           $set:{
+                owner:{$arrayElemAt:["$owner",0]}
+           }
+         },
+         {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"comment",
+                as:"likedComment"
+            }
+         },
+         {
+            $addFields:{
+                numberOfLikes:{$size:"$likedComment"}
+            }
+         },
+         {
+            $project:{
+                content:1,
+                owner:1,
+                numberOfLikes:1
+            }
+         }
+    ])
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,comments,"comments fetched successfully")
+        )
 })
 
 const addComment = asyncHandler(async (req, res) => {
@@ -46,7 +109,46 @@ const addComment = asyncHandler(async (req, res) => {
             )
         )
 })
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+    const {commentId} = req.params;
+    const{content} = req.body;
+    if(!content || content.trim("")===""){
+        throw new ApiError(400,"comment can not empty")
+    }
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"Invalid comment Id")
+    }
+    if(!commentId){
+        throw new ApiError(400,"comment id is required")
+    };
+    const comment = await Comment.findOneAndUpdate(
+        {
+            _id:new mongoose.Types.ObjectId(commentId),
+            owner:req.user?._id
+        },
+        {
+            $set:{
+                content:content
+            }
+        },
+        {
+           new:true
+        }
+    )
+   
+    if(!comment){
+        throw new ApiError(400,"something went wrong while fetch!!");
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200,comment,"successfully comment update:")
+        )
+})
 module.exports = {
     getVideoComments,
     addComment,
+    updateComment
+
 }
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -2,10 +2,12 @@ const {Router} = require("express")
 const router = Router()
 const {
     getVideoComments,
-    addComment
+    addComment,
+    updateComment
 } = require("../controllers/comment.controller.js")
 const { verifyJWT } = require("../middlewares/auth.middleware.js")
 
+router.route("/:videoId/allComments").get(getVideoComments)
 router.route("/:videoId").post(verifyJWT,addComment)
-
+router.route("/:commentId/updateComment").post(verifyJWT,updateComment)
 module.exports = router
